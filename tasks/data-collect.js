0eed5b3b0e33d3ba354218ed7b96cc37d4fd5511
claudiocalautti <claudio@creativefrontend.com>
2015-05-16 10:59:30 +0100

    Modified grunt task data-collect.js in order to get only data related to current year and save data only if DST is observed.

--- tasks/data-collect.js
+++ tasks/data-collect.js
@@ -7,6 +7,8 @@ module.exports = function (grunt) {
 	grunt.registerTask('data-collect', '4. Collect all data from zdump(8) into a single json file.', function (version) {
 		version = version || 'latest';
 
+		var currentYear = new Date().getFullYear();
+
 		var files = grunt.file.expand({ filter : 'isFile', cwd : 'temp/zdump/' + version }, '**/*.zdump'),
 			data  = [];
 
@@ -16,25 +18,40 @@ module.exports = function (grunt) {
 				untils  = [],
 				offsets = [];
 
-			lines.forEach(function (line) {
-				var parts  = line.split(/\s+/),
-					format = "MMM D HH:mm:ss YYYY",
-					utc    = moment.utc(parts.slice(2, 6).join(' '), format),
-					local  = moment.utc(parts.slice(9, 13).join(' '), format);
+			lines.forEach(function (line, i) {
 
-				if (parts.length < 13) { return; }
+				// line = Europe/London  Sun Mar 29 01:00:00 2015 UTC = Sun Mar 29 02:00:00 2015 BST isdst=1
 
-				offsets.push(+utc.diff(local, 'minutes', true).toFixed(4));
-				untils.push(+utc);
-				abbrs.push(parts[13]);
+				var parts  = line.split(/\s+/); // ["Europe/London", "Sun", "Mar", "29", "01:00:00", "2015", "UTC", "=", "Sun", "Mar", "29", "02:00:00", "2015", "BST", "isdst=1"]
+
+				// Push only current year.
+				if (Number(parts[5]) === Number(currentYear)) {
+
+					var format = "MMM D HH:mm:ss YYYY",
+						utc    = moment.utc(parts.slice(2, 6).join(' '), format), // "Mar", "29", "01:00:00", "2015"
+						local  = moment.utc(parts.slice(9, 13).join(' '), format); // Mar", "29", "02:00:00", "2015"
+
+					if (parts.length < 13) {
+						return;
+					}
+
+					offsets.push(+utc.diff(local, 'minutes', true).toFixed(4));
+					untils.push(+utc);
+					abbrs.push(parts[13]);
+				}
 			});
 
-			data.push({
-				name    : file.replace(/\.zdump$/, ''),
-				abbrs   : abbrs,
-				untils  : untils,
-				offsets : offsets
-			});
+			// Push only if observe DST.
+			if (untils.length > 0) {
+
+				data.push({
+					name    : file.replace(/\.zdump$/, ''),
+					abbrs   : abbrs,
+					untils  : untils,
+					offsets : offsets
+				});
+			}
+
 		});
 
 		grunt.file.mkdir('temp/collect');
